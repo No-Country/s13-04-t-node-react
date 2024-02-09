9de741f774e7e2cdef742cefcee17b942119fb8d
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-02-09 17:44:35 -0300

    init app

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+}
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.use(express.json())
+app.use(cors(corsOptions))
+
+app.get("/api/health-check", async (req, res) => {
+    res.status(200).send("Stable");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+})
