f82b41a1199aece4ccc5736a2e523f5012160587
David Castro <davidernesto.castroanaya@gmail.com>
2024-02-12 09:34:08 -0600

    Adding Garage CRUD

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,14 +1,11 @@
 import { Router } from "express";
 import userRoute from "./user.js"
+import garageRoute from './garage.js'
 
 const route=Router()
 
 
-
 route.use("/user",userRoute)
-
-
-
-
+route.use("/garage", garageRoute)
 
 export default route;
