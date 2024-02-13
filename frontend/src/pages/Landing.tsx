55cbed9ace7975db64f53ce7323db76b417ae84d
John Pablo Valverde <80083262+johnpablo7@users.noreply.github.com>
2024-02-13 13:06:41 -0500

    fix app and add landing router

---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,3 @@
+export default function Landing(): JSX.Element {
+  return <div>Hola bebe</div>;
+}
--- frontend/src/routes/LazyRoutes.ts
+++ frontend/src/routes/LazyRoutes.ts
@@ -1,19 +1,22 @@
-import { lazy } from 'react'
+import { lazy } from 'react';
 
 const Onboarding: React.LazyExoticComponent<() => JSX.Element> = lazy(
   async () => await import('../pages/Onboarding')
-)
+);
 const Login: React.LazyExoticComponent<() => JSX.Element> = lazy(
   async () => await import('../pages/Login')
-)
+);
 const Register: React.LazyExoticComponent<() => JSX.Element> = lazy(
   async () => await import('../pages/Register')
-)
+);
+const Landing: React.LazyExoticComponent<() => JSX.Element> = lazy(
+  async () => await import('../pages/Landing')
+);
 const Home: React.LazyExoticComponent<() => JSX.Element> = lazy(
   async () => await import('../pages/Home')
-)
+);
 const NotFound: React.LazyExoticComponent<() => JSX.Element> = lazy(
   async () => await import('../pages/NotFound')
-)
+);
 
-export { Onboarding, Home, NotFound, Login, Register }
+export { Onboarding, Landing, NotFound, Login, Register, Home };
