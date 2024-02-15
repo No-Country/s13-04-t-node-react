6536ffb7cceab0a796a8ee5ace6cc4f1a2c252d2
LautaroLD <duranlautarogabriel@gmail.com>
2024-02-15 16:24:20 -0300

    add new parking page

--- frontend/src/routes/AuthRouter.tsx
+++ frontend/src/routes/AuthRouter.tsx
@@ -1,11 +1,11 @@
-import { Route, Routes } from 'react-router-dom';
-import { Home, NotFound } from './LazyRoutes';
+import { Route, Routes } from 'react-router-dom'
+import { Home, NotFound } from './LazyRoutes'
 
-export default function AuthRouter() {
+export default function AuthRouter () {
   return (
     <Routes>
       <Route path='/home' element={<Home />} />
       <Route path='*' element={<NotFound />} />
     </Routes>
-  );
+  )
 }
