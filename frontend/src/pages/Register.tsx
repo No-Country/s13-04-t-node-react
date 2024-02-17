957919e22951af668c99f7073696ab61c8293426
Pablo Swis <94465269+Pabloswis@users.noreply.github.com>
2024-02-17 15:38:05 -0300

    fix register useParams

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,27 +1,27 @@
-import { useLocation, useParams } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 import UserRegisterForm from "../components/UserRegisterForm"
 
-export default function Register (): JSX.Element {
-  const {state} = useLocation()
-  const {form} = state
-  
+export default function Register(): JSX.Element {
+  const { state } = useLocation()
+  const { form } = state
+
   return (
     <main className="p-4">
-    {form ==='conductor' ?
-    (
-      <>
-      <h1>REGÍSTRATE COMO CONDUCTOR</h1>
-      <p>Completa tus datos personales</p>
-      </>
-    ):
-    (
-      <>
-      <h1>REGÍSTRATE COMO ESTACIONAMIENTO</h1>
-      <p>Completa tus datos personales</p>
-      </>
-    )
-  }
-    <UserRegisterForm/>
+      {form === 'conductor' ?
+        (
+          <>
+            <h1>REGÍSTRATE COMO CONDUCTOR</h1>
+            <p>Completa tus datos personales</p>
+          </>
+        ) :
+        (
+          <>
+            <h1>REGÍSTRATE COMO ESTACIONAMIENTO</h1>
+            <p>Completa tus datos personales</p>
+          </>
+        )
+      }
+      <UserRegisterForm />
     </main>
   )
 }
