f1017dd52aa8cfb47d2b5ddc9258af1363882c86
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-02-18 19:01:39 -0300

    Errors schemas

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -7,6 +7,38 @@ const options = {
       info: { title: "EstacionAPP API", version: "1.0.0" },
       components: {
         schemas: {
+            Car: {
+                type: "object",
+                properties: {
+                    id: {
+                        type: "string",
+                        format: "uuid",
+                        example: "123e4567-e89b-12d3-a456-426614174000",
+                        description: "ID del coche (UUID)"
+                    },
+                    brand: {
+                        type: "string",
+                        example: "Toyota",
+                        description: "Marca del coche"
+                    },
+                    model: {
+                        type: "string",
+                        example: "Corolla",
+                        description: "Modelo del coche"
+                    },
+                    plate: {
+                        type: "string",
+                        example: "ABC123",
+                        description: "Placa del coche"
+                    },
+                    color: {
+                        type: "string",
+                        example: "Rojo",
+                        description: "Color del coche"
+                    }
+                },
+                required: ["brand", "model", "plate", "color"]
+            },
             ErrorSchemas:{
                 Error: {
                     type : "object",
@@ -48,39 +80,67 @@ const options = {
                         }
                     }
                 },
-            },
-            Car: {
-                type: "object",
-                properties: {
-                    id: {
-                        type: "string",
-                        format: "uuid",
-                        example: "123e4567-e89b-12d3-a456-426614174000",
-                        description: "ID del coche (UUID)"
-                    },
-                    brand: {
-                        type: "string",
-                        example: "Toyota",
-                        description: "Marca del coche"
-                    },
-                    model: {
-                        type: "string",
-                        example: "Corolla",
-                        description: "Modelo del coche"
-                    },
-                    plate: {
-                        type: "string",
-                        example: "ABC123",
-                        description: "Placa del coche"
-                    },
-                    color: {
-                        type: "string",
-                        example: "Rojo",
-                        description: "Color del coche"
+                BadRequest: {
+                    type : "object",
+                    properties: {
+                        error: {
+                            type: "boolean",
+                            example: true,
+                            description: "Indica si ocurrio un error"
+                        },
+                        message: {
+                            type: "string",
+                            example: "Bad Request",
+                            description: "Indica el mensaje de error"
+                        },
+                        statusCode: {
+                            type: "integer",
+                            example: 400,
+                            description: "Indica el codigo de status"
+                        }
                     }
                 },
-                required: ["brand", "model", "plate", "color"]
-            }
+                AlreadyExist: {
+                    type : "object",
+                    properties: {
+                        error: {
+                            type: "boolean",
+                            example: true,
+                            description: "Indica si ocurrio un error"
+                        },
+                        message: {
+                            type: "string",
+                            example: "Resource already exist",
+                            description: "Indica el mensaje de error"
+                        },
+                        statusCode: {
+                            type: "integer",
+                            example: 403,
+                            description: "Indica el codigo de status"
+                        }
+                    }
+                },
+                Unauthorized: {
+                    type : "object",
+                    properties: {
+                        error: {
+                            type: "boolean",
+                            example: true,
+                            description: "Indica si ocurrio un error"
+                        },
+                        message: {
+                            type: "string",
+                            example: "Unauthorized",
+                            description: "Indica el mensaje de error"
+                        },
+                        statusCode: {
+                            type: "integer",
+                            example: 401,
+                            description: "Indica el codigo de status"
+                        }
+                    }
+                },
+            },
         }
     }
     },
