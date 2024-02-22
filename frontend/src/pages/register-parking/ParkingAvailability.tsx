44b1d492d4b4efec56f3d0354c3276fc326a5ca9
Yein E <yeinneg@gmail.com>
2024-02-22 17:43:07 -0300

    lo olvide

--- frontend/src/pages/register-parking/ParkingAvailability.tsx
+++ frontend/src/pages/register-parking/ParkingAvailability.tsx
@@ -1,89 +1,87 @@
-import { type SubmitHandler, useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
-import { HeaderLanding } from '../../components/landing/HeaderLanding';
+import { type SubmitHandler, useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { HeaderLanding } from "../../components/landing/HeaderLanding";
 
 interface Inputs {
-	abailavility: string;  // revisar nombre en back
-	price: string;         // revisar nombre en back
-
+  abailavility: string; // revisar nombre en back
+  price: string; // revisar nombre en back
 }
 
 export default function ParkingAvailability(): JSX.Element {
-    const {
-		register,
-		watch,
-		handleSubmit,
-		setValue,
-		formState: { errors },
-	} = useForm<Inputs>();
-	const onSubmit: SubmitHandler<Inputs> = (data) => {
-		console.log(data);
-	};
+  const {
+    register,
+    watch,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
   return (
     <>
-    <HeaderLanding />
-    <main className="px-4 py-10">
-      {/* Titulos de la pantalla */}
-      <h1 className='uppercase text-xl font-normal'>
-					AÑADIR ESTACIONAMIENTO
-				</h1>
-				<p>Completa los datos de tu estacionamiento</p>
+      <HeaderLanding />
+      <main className="px-4 py-10">
+        {/* Titulos de la pantalla */}
+        <h1 className="uppercase text-xl font-normal">
+          AÑADIR ESTACIONAMIENTO
+        </h1>
+        <p>Completa los datos de tu estacionamiento</p>
 
-     {/* Inputs de la pantalla */}
-    <form
-					className='flex-col flex gap-3 text-sm mt-10'
-					onSubmit={handleSubmit(onSubmit)}
-				>
-
-            <label className="flex-col flex mb-4 mt-8">
-             Cantidad de plazas disponibles
-              <input
-                className="px-4 py-2 border border-[#D58418] rounded outline-none mt-2"
-                placeholder="Ingresa la cantidad de lugares"
-                type="text"
-                id="abailavility"
-                {...register("abailavility", { required: true })}
-              />
-              {errors.abailavility != null && (
-                <span className="text-red-500">
-                  La cantidad de lugares es obligatoria
-                </span>
-              )}
-            </label>
-            
-           
-              <label className="flex-col flex mb-4 mt-8">
-              Valor hora
-              <input
-                className="px-4 py-2 border border-[#D58418] rounded outline-none mt-2"
-                placeholder="Ingresa el valor de la hora de estacionamiento"
-                type="text"
-                id="price"
-                {...register("price", { required: true })}
-              />
-              {errors.price != null && (
-                <span className="text-red-500">
-                  El valor de la hora de estacionamiento es obligatoria
-                </span>
-              )}
-            </label>
-
-            {/* Confirmacion de reservas */}
-            <div>
+        {/* Inputs de la pantalla */}
+        <form
+          className="flex-col flex gap-3 text-sm mt-10"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <label className="flex-col flex mb-4 mt-8">
+            Cantidad de plazas disponibles
             <input
-            type="radio"
-            id="notificationYes"
-            value="yes"
-            // checked={notificationPreference === 'yes'}
-            // onChange={() => setNotificationPreference('yes')}
-            className="mr-2"
-          />
-          <label htmlFor="notificationYes">Deseo recibir una notificación cuando un conductor reserve en mi estacionamiento para poder confirmar o rechazar la misma.</label>
+              className="px-4 py-2 border border-[#D58418] rounded outline-none mt-2"
+              placeholder="Ingresa la cantidad de lugares"
+              type="text"
+              id="abailavility"
+              {...register("abailavility", { required: true })}
+            />
+            {errors.abailavility != null && (
+              <span className="text-red-500">
+                La cantidad de lugares es obligatoria
+              </span>
+            )}
+          </label>
 
-            </div>
+          <label className="flex-col flex mb-4 mt-8">
+            Valor hora
+            <input
+              className="px-4 py-2 border border-[#D58418] rounded outline-none mt-2"
+              placeholder="Ingresa el valor de la hora de estacionamiento"
+              type="text"
+              id="price"
+              {...register("price", { required: true })}
+            />
+            {errors.price != null && (
+              <span className="text-red-500">
+                El valor de la hora de estacionamiento es obligatoria
+              </span>
+            )}
+          </label>
 
-             
-            {/* Botones de acción */}
+          {/* Confirmacion de reservas */}
+          <div>
+            <input
+              type="radio"
+              id="notificationYes"
+              value="yes"
+              // checked={notificationPreference === 'yes'}
+              // onChange={() => setNotificationPreference('yes')}
+              className="mr-2"
+            />
+            <label htmlFor="notificationYes">
+              Deseo recibir una notificación cuando un conductor reserve en mi
+              estacionamiento para poder confirmar o rechazar la misma.
+            </label>
+          </div>
+
+          {/* Botones de acción */}
 
           <button
             className="py-2 text-center bg-[#D58418] rounded-3xl font-semibold mt-8 rounded-3xl"
@@ -100,11 +98,8 @@ export default function ParkingAvailability(): JSX.Element {
               Cancelar
             </button>
           </Link>
-
-    </form>
-
-      
-    </main>
+        </form>
+      </main>
     </>
   );
 }
