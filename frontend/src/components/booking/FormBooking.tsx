7870fbdde52d806f41b5324f80797544c1811b3a
Pablo Swis <94465269+Pabloswis@users.noreply.github.com>
2024-02-22 18:05:15 -0300

    modularizacion del componente formBooking

--- frontend/src/components/booking/FormBooking.tsx
+++ frontend/src/components/booking/FormBooking.tsx
@@ -1,140 +1,49 @@
 import { Link, useParams } from "react-router-dom";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { useEffect, useState } from "react";
-import { FaHeart } from 'react-icons/fa';
 import { garageService } from "../../services/garage";
 import { IGarage } from "../../types/garage";
+import Carousel from "./Carousel";
+// import { useCurrentUser } from "../../hooks/auth";
+import Header from "./Header";
+import Schedule from "./Schedule";
+import { schedule } from "./MockSchedule";
 
 // id1 ='8b0cec74-6355-4949-ac20-70b06df8313e'
 // id2 ='b7b7d344-fca3-4fae-a049-a914cf37fd67'
 
 export const FormBooking = () => {
-  const url = useParams()
-  //traer datos del usuario
+  const { idGaraje } = useParams();
+  // datos del usuario logueado
   // const user = useCurrentUser();
-
-
-  //traer los datos del garaje
+  //datos del garaje a reservar
   const [garaje, setGaraje] = useState<IGarage | undefined>()
 
+
   useEffect(() => {
-    if (url.idGaraje) {
-      garageService.getById(url.idGaraje)
-        .then(res => { setGaraje(res) })
+    if (idGaraje) {
+      garageService.getById(idGaraje)
+        .then(res => {
+          setGaraje(res)
+          console.log(res)
+        })
     }
-    // console.log(url.idGaraje)
-  }, [url])
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [favorites, setFavorites] = useState<number[]>([]);
-
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    adaptiveHeight: true,
-  };
-  const addToFavorites = () => {
-    //agregar el garaje a favoritos
-  };
+  }, [idGaraje])
 
 
   return (
     <div className="px-4 py-8 h-screen flex flex-col gap-6">
       {/* Carrusel de imagenes */}
-      <section className="bg-gray-300 rounded-md relative">
-        <Slider {...settings}>
-          <div className="relative">
-            <img
-              src="https://tse4.mm.bing.net/th?id=OIP.wkFW3sPbRMmNXoKnAKyelwHaFj&pid=Api&P=0&h=180"
-              alt="Imagen de un garaje 1"
-              className="h-auto max-h-56 w-full rounded-md"
-            />
-            <button
-              className="absolute top-2 right-2 text-red-500"
-              onClick={() => addToFavorites()}
-            >
-              <FaHeart color={favorites ? 'red' : 'gray'} />
-            </button>
-          </div>
-          <div className="relative">
-            <img
-              src="https://tse2.mm.bing.net/th?id=OIP.CTTp75yu1mTgpRxM4sGK5gHaFj&pid=Api&P=0&h=180"
-              alt="Imagen de un garaje 2"
-              className="h-auto max-h-56 w-full rounded-md"
-            />
-            <button
-              className="absolute top-2 right-2 text-red-500"
-              onClick={() => addToFavorites()}
-            >
-              <FaHeart color={favorites ? 'red' : 'gray'} />
-            </button>
-          </div>
-        </Slider>
-      </section>
-
+      <Carousel
+        images={[
+          "https://tse4.mm.bing.net/th?id=OIP.wkFW3sPbRMmNXoKnAKyelwHaFj&pid=Api&P=0&h=180",
+          "https://tse2.mm.bing.net/th?id=OIP.CTTp75yu1mTgpRxM4sGK5gHaFj&pid=Api&P=0&h=180",
+          "https://tse1.mm.bing.net/th?id=OIP.8HjWX5curav0AmsxCvY-pwAAAA&pid=Api&P=0&h=180"
+        ]}
+      />
       {/* encabezado */}
-      <div className="grid grid-cols-4 gap-1 items-center">
-        <section className="col-span-3 w-full gap-1">
-          <p className="text-xl uppercase">{garaje?.name} </p>
-          <p className="text-lg">{garaje?.address}</p>
-        </section>
-        <section className="bg-[#5D2B2C] col-span-1 h-[52px] rounded-lg text-center flex items-center justify-center text-2xl text-white">
-          4,5
-        </section>
-      </div>
-      {/* precio */}
-      <div className="text-center p-4 text-2xl font-medium rounded-lg text-black bg-[#D58418]">
-        <span className="">${garaje?.price} x Hora </span>
-      </div>
+      {garaje && <Header name={garaje.name} address={garaje.address} price={garaje.price} />}
       {/* horairos */}
-      <section className="w-full">
-        <ul className="grid grid-cols-2 ">
-          <li className="text-left">
-            <strong>Lunes:</strong>
-          </li>
-          <li className="text-left">00:00 - 00:00</li>
-          <li className="text-left">
-            <strong>Martes:</strong>
-          </li>
-          <li className="text-left">00:00 - 00:00</li>
-          <li className="text-left">
-            <strong>Miércoles:</strong>
-          </li>
-          <li className="text-left">00:00 - 00:00</li>
-          <li className="text-left">
-            <strong>Jueves:</strong>
-          </li>
-          <li className="text-left">00:00 - 00:00</li>
-          <li className="text-left">
-            <strong>Viernes:</strong>
-          </li>
-          <li className="text-left">00:00 - 00:00</li>
-          <li className="text-left">
-            <strong>Sábado:</strong>
-          </li>
-          <li className="text-left">00:00 - 00:00</li>
-          <li className="text-left">
-            <strong>Domingo:</strong>
-          </li>
-          <li className="text-left">00:00 - 00:00</li>
-        </ul>
-        <div className="flex items-center py-4 text-black">
-          <img
-            className="h-6 w-6"
-            src="/images/reserva-icon.svg"
-            alt="reserva icon"
-          />
-          <Link to="/reservar/disponibilidad">
-            <span>Ver Disponibilidad</span>
-          </Link>
-        </div>
-
-      </section>
+      <Schedule schedule={schedule} />
       {/* mapa */}
       <section className="bg-[#FFE9CC] w-328 h-[72px] rounded-4 py-8 px-0 gap-x-24 flex items-center justify-center">
         <Link to="/#" className="flex items-center space-x-2 text-black">
