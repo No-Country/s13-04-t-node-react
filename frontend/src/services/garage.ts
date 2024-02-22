de53e042ca4d29ea2163f79cadbc61765bda36f3
Pablo Swis <94465269+Pabloswis@users.noreply.github.com>
2024-02-22 15:25:09 -0300

    agrega metodo getById

--- frontend/src/services/garage.ts
+++ frontend/src/services/garage.ts
@@ -1,11 +1,18 @@
-import { client } from '../config/client';
-import { IGarage } from '../types/garage';
+import { client } from "../config/client";
+import { IGarage } from "../types/garage";
 
 export const garageService = {
   async list() {
-    const res = await client.get<{ garages: IGarage[] }>('/garages', {
+    const res = await client.get<{ garages: IGarage[] }>("/garages", {
       params: {},
     });
+    // console.log(res.data.garages);
     return res.data.garages;
+  },
+  async getById(idGarage: string) {
+    const res = await client.get<{ garage: IGarage }>(`/garages/${idGarage}`, {
+      params: {},
+    });
+    return res.data.garage;
   },
 };
