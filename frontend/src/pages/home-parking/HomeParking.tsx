5a9ab5ed23f82e2bff5c4e3f27affe294a04ae48
Yein E <yeinneg@gmail.com>
2024-03-01 14:50:36 -0300

    home parking user

--- frontend/src/pages/home-parking/HomeParking.tsx
+++ frontend/src/pages/home-parking/HomeParking.tsx
@@ -1,3 +1,69 @@
+import { ParkingReservatiosCard } from "../../components/parkingReservation/ParkingReservationCard";
+import { useCurrentUser } from "../../hooks/auth";
+
 export const HomeParking = () => {
-  return <div>HomeParking</div>;
+  const user = useCurrentUser();
+  return (
+    <>
+      <div className="p-4 pb-0">
+        <h1 className="uppercase text-2xl pb-8">¡HOLA, {user.name}!</h1>
+        <span className=" font-semibold">Reservas pendientes</span>
+      </div>      
+      <ParkingReservatiosCard
+        showDate={false}
+        showChat={false}
+        showImgUser={false}
+      />
+      <div className="p-4">
+        <div className=" mt-4">
+          <span className="font-semibold ">Tus estadísticas semanales</span>
+        </div>
+        <div className="flex flex-row justify-between mt-4">
+          <div className="text-center ">
+            <div className="flex items-center justify-center bg-[#5D2B2C] text-white rounded-md text-center px-6 py-4 text-lg">
+              <span>4,5</span>
+            </div>
+            <span>Puntos</span>
+          </div>
+          <div className="text-center ">
+            <div className="flex items-center justify-center bg-[#D58418] rounded-md text-center px-4 py-3 text-lg">
+              <img src="/images/arrowUpFit.svg" alt="arrow up" />
+            </div>
+            <span>ingresos</span>
+          </div>
+          <div className="text-center">
+            <div className="flex items-center justify-center bg-[#D58418] font-semibold rounded-md text-center px-6 py-4 text-lg ">
+              <span>$ 10540</span>
+            </div>
+            <span>Ingreso semanal</span>
+          </div>
+        </div>
+      </div>
+      <div className="p-4">
+        <div className="flex flex-row justify-between">
+          <div className="text-center">
+            <div className="flex items-center justify-center bg-[#5D2B2C] text-white rounded-md text-center px-6 py-4 text-lg">
+              <span>15</span>
+            </div>
+            <span>Nuevos conductores</span>
+          </div>
+          <div className="text-center ">
+            <div className="flex items-center justify-center bg-[#5D2B2C] text-white rounded-md text-center px-6 py-4 text-lg">
+              <span>10</span>
+            </div>
+            <span>Nuevos conductores</span>
+          </div>
+        </div>
+      </div>
+      <div className="p-4">
+        <h2 className=" font-semibold">Ganancias del mes</h2>
+        <h2 className="mt-2 text-lg">Febrero 2024</h2>
+        <img
+          src="/images/barGraph.svg"
+          alt="estadisticas"
+          className="mt-4 w-full h-177"
+        />
+      </div>
+    </>
+  );
 };
