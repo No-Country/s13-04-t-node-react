04e6472e4474273e91a1330b8665065741983b7c
John Pablo Valverde <80083262+johnpablo7@users.noreply.github.com>
2024-03-04 16:04:28 -0500

    fixed

--- frontend/src/pages/booking-driver/Booking.tsx
+++ frontend/src/pages/booking-driver/Booking.tsx
@@ -3,20 +3,16 @@ import { FormBooking } from '../../components/booking/FormBooking';
 import { HeaderUser } from '../../components/shared/HeaderUser';
 import { useParams } from 'react-router-dom';
 
-
 export default function Booking() {
   const user = useCurrentUser();
   const { idGaraje } = useParams();
-
 
   return (
     <>
       <HeaderUser />
       {user.role === 'user' ? (
         <div className=''>
-          {idGaraje &&
-            <FormBooking garajeId={idGaraje} />
-          }
+          {idGaraje && <FormBooking garajeId={idGaraje} userId={''} />}
         </div>
       ) : (
         <h1>No estas registrado</h1>
