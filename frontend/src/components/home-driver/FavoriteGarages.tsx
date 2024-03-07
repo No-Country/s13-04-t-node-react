6b52f3f66e34e3c240b44570135971fcf86459c7
LautaroLD <duranlautarogabriel@gmail.com>
2024-03-06 21:28:51 -0300

    Add loading state to FavoriteGarages and ParticularGarages components

--- frontend/src/components/home-driver/FavoriteGarages.tsx
+++ frontend/src/components/home-driver/FavoriteGarages.tsx
@@ -2,22 +2,30 @@ import useSWR from 'swr';
 import { CardGarage } from './CardGarage';
 import { Link } from 'react-router-dom';
 import { favoriteService } from '../../services/favorites';
+import { useEffect, useState } from 'react';
+import { LoadingIcon } from '../shared/LoadingIcon';
 
 export const FavoriteGarages = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const { data: favorites } = useSWR(['favorite-garages'], () =>
     favoriteService.list()
   );
   const filteredGarages = favorites;
-
+  useEffect(() => {
+    filteredGarages && setIsLoading(false);
+  }, [filteredGarages]);
   return (
     <div className='pb-6'>
       <h2 className='font-semibold pb-2 text-xl'>Estacionamientos favoritos</h2>
-      <ul className='flex overflow-x-auto items-center w-auto scrollbar-hidden gap-x-4'>
-        {filteredGarages?.map((favorite) => (
-          <Link key={favorite.id} to={`/reservar/${favorite.garage.id}`}>
-            <CardGarage garage={favorite.garage} />
-          </Link>
-        ))}
+      <ul className='flex overflow-x-auto items-center w-auto scrollbar-hidden gap-x-4 min-h-[100px]'>
+        {isLoading && <LoadingIcon width={50} />}
+
+        {!isLoading &&
+          filteredGarages?.map((favorite) => (
+            <Link key={favorite.id} to={`/reservar/${favorite.garage.id}`}>
+              <CardGarage garage={favorite.garage} />
+            </Link>
+          ))}
       </ul>
     </div>
   );
--- frontend/src/components/home-driver/ParticularGarages.tsx
+++ frontend/src/components/home-driver/ParticularGarages.tsx
@@ -2,23 +2,30 @@ import useSWR from 'swr';
 import { garageService } from '../../services/garage';
 import { CardGarage } from './CardGarage';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { LoadingIcon } from '../shared/LoadingIcon';
 
 export const ParticularGarages = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const { data: garages } = useSWR(['particular-garages'], () =>
     garageService.listRecomended()
   );
   //filtrar solo garajes con rating
   const filteredGarages = garages;
-
+  useEffect(() => {
+    filteredGarages && setIsLoading(false);
+  }, [filteredGarages]);
   return (
     <div>
       <h2 className='font-semibold pb-2 text-xl'>Garajes recomendados</h2>
-      <ul className='flex overflow-x-auto items-center w-auto scrollbar-hidden gap-x-4'>
-        {filteredGarages?.map((garage) => (
-          <Link key={garage.id} to={`/reservar/${garage.id}`}>
-            <CardGarage garage={garage} />
-          </Link>
-        ))}
+      <ul className='flex overflow-x-auto items-center w-auto scrollbar-hidden gap-x-4 min-h-[100px]'>
+        {isLoading && <LoadingIcon width={50} />}
+        {!isLoading &&
+          filteredGarages?.map((garage) => (
+            <Link key={garage.id} to={`/reservar/${garage.id}`}>
+              <CardGarage garage={garage} />
+            </Link>
+          ))}
       </ul>
     </div>
   );
